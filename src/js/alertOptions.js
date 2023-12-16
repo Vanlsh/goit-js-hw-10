82825d55d2382982daccf0591a65feb220e8d475
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2023-12-16 22:02:48 +0200

    first task

---
+++ src/js/alertOptions.js
@@ -0,0 +1,14 @@
+export const alertOptions = {
+  error: {
+    title: 'Error',
+    message: 'Please choose a date in the future',
+    color: '#EF4040',
+    position: 'topRight',
+    icon: 'icon-octagon',
+    iconText: '',
+    timeout: 5000,
+    titleColor: '#fff',
+    messageColor: '#fff',
+    iconColor: '#fff',
+  },
+};
